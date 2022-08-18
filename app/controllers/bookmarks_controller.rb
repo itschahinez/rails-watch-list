e0b8c17d8630c6e59a26c6b34c6542e7e2acb2b4
Chahinez Haddad <mackychahinez@gmail.com>
2022-08-18 17:13:30 +0200

    still have CSS to do

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,25 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[create]
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path
+  end
+
+  private
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+
+url = 'http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key>'
+movie_serialized = URI.open(url).read
+movie_parsed = JSON.parse(movie_serialized)
+
+Movie.delete_all
+puts "Starting..."
+
+movies = movie_parsed["results"]
+
+movies.each do |movie|
+  @movie = Movie.new
+  @movie.title = movie["original_title"]
+  @movie.overview = movie["overview"]
+  @movie.rating = movie["vote_average"]
+  @movie.poster_url = movie["poster_path"]
+
+  @movie.save!
+end
+
+p "all movies created"
+List.delete_all
+p "Creating lists"
+ lists = ['Drama', 'Love story', 'Horror', 'Thriller', 'Comedy']
+
+ lists.each_with_index do |genre, index|
+  @list = List.create(name: genre).save!
+  "Creating list #{index}"
+ end
+
+ p "Created all lists"
